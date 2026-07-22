a67f26a06afd6944365b752e430d63032894ca0d
Bob Rockefeller <bob@bobrockefeller.com>
2026-07-22 13:32:12 -0400

    Tuning up NeoVIM

---
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      {
+        "<leader>gg",
+        "<cmd>LazyGit<cr>",
+        desc = "Open LazyGit",
+      },
+    },
+  },
+}
