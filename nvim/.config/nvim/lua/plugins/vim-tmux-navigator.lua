8ab2a13d04819e2a1a43b02e6d3aec80038797cd
Bob Rockefeller <bob@bobrockefeller.com>
2026-07-24 14:00:38 -0400

    Another Omarchy change

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,10 +1,18 @@
+local function navigate(direction, tmux_direction)
+  if vim.fn.winnr(direction) ~= vim.fn.winnr() then
+    vim.cmd("wincmd " .. direction)
+  elseif vim.env.TMUX then
+    vim.fn.system({ "tmux", "select-pane", tmux_direction })
+  end
+end
+
 return {
 	"christoomey/vim-tmux-navigator",
 	lazy = false,
 	keys = {
-		{ "<C-A-Left>", "<cmd>TmuxNavigateLeft<CR>", desc = "Navigate left" },
-		{ "<C-A-Right>", "<cmd>TmuxNavigateRight<CR>", desc = "Navigate right" },
-		{ "<C-A-Down>", "<cmd>TmuxNavigateDown<CR>", desc = "Navigate down" },
-		{ "<C-A-Up>", "<cmd>TmuxNavigateUp<CR>", desc = "Navigate up" },
+		{ "<C-A-Left>", function() navigate("h", "-L") end, desc = "Navigate left" },
+		{ "<C-A-Right>", function() navigate("l", "-R") end, desc = "Navigate right" },
+		{ "<C-A-Down>", function() navigate("j", "-D") end, desc = "Navigate down" },
+		{ "<C-A-Up>", function() navigate("k", "-U") end, desc = "Navigate up" },
 	},
 }
